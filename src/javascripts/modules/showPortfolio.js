7a3d5db9cc9651f030bc63c564a8ac5de3c6e376
Luca Joshua Mosca <joshuarte@hotmail.it>
2017-07-27 16:33:58 +0200

    porting ok - to reset ALL

---
+++ src/javascripts/modules/showPortfolio.js
@@ -0,0 +1,31 @@
+export default class showPortfolio {
+    constructor(el) {
+
+        // console.log(document.getElementsByClassName("right"));
+
+        // // First we detect the click event
+        // document.getElementsByClassName("right").addEventListener('click', function () {
+        //     /* if (this.classList.contains('slideInRight')) {
+        //         this.classList.remove('slideInRight');
+        //         this.classList.add('slideOutRight');
+        //     } else {
+        //         this.classList.remove('slideOutRight');
+        //         this.classList.add('slideInRight');
+        //     } */
+        //     this.classList.toggle('slideOutRight');
+        // });
+
+
+
+
+        /* var sideNav = document.getElementsById("right") */
+        this.el = el
+        console.log('Show Portfolio module loaded');
+
+        var sideNav = document.querySelectorAll('.right')[0];
+
+        this.el.addEventListener('click', function(event){
+            sideNav.classList.toggle("slideOutRight");
+        })
+    }
+}
