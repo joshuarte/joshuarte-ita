199c9ea437eade884d0b2b431f6d14d8469fd631
Luca Joshua Mosca <joshuarte@hotmail.it>
2017-07-28 18:14:41 +0200

    all ok, privacy da fare e modal

--- src/javascripts/modules/ctrl.js
+++ src/javascripts/modules/ctrl.js
@@ -1,6 +1,28 @@
+import 'velocity-animate';
+
+
 export default class Ctrl {
   constructor(el) {
     this.el = el
     console.log('Controller module loaded');
+
+    var width = 100,
+      perfData = window.performance.timing, // The PerformanceTiming interface represents timing-related performance information for the given page.
+      EstimatedTime = -(perfData.loadEventEnd - perfData.navigationStart),
+      time = parseInt((EstimatedTime / 500) % 60) * 100;
+
+    var loader = document.getElementsByClassName("loader");
+    var leftMask = document.getElementById("left-mask");
+    var rightMask = document.getElementById("right-mask");
+
+    window.Velocity(loader, { height: width + "%" }, time)
+      .then(function () {
+        window.Velocity(leftMask, { width: width - "%" }, 300)
+          .then(function () {
+            window.Velocity(rightMask, { width: width - "%" }, 300);
+            window.Velocity(loader, { width: 0 }, 300);
+          });
+      });
+
   }
 }
---
+++ src/javascripts/modules/scrollTop.js
@@ -0,0 +1,27 @@
+import jump from 'jump.js'
+
+export default class scrollTop {
+    constructor(el) {
+        
+        const easeInOutQuad = (t, b, c, d) => {
+            t /= d / 2
+            if (t < 1) return c / 2 * t * t + b
+            t--
+            return -c / 2 * (t * (t - 2) - 1) + b
+        }
+
+        this.el = el
+        console.log('scrollTop module loaded');
+
+        this.el.addEventListener('click', function (event) {
+            jump('#page-top', {
+                duration: 1000,
+                offset: 0,
+                callback: undefined,
+                easing: easeInOutQuad,
+                a11y: false
+            })
+        });
+
+    }
+}
