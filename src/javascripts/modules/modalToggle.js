1f13ff8b97e768437cd34fdc9fdbb820df7b37e2
Luca Joshua Mosca <joshuarte@hotmail.it>
2017-08-01 17:17:51 +0200

    privacy ok - da aggiustare i class names e id

---
+++ src/javascripts/modules/modalToggle.js
@@ -0,0 +1,13 @@
+export default class modalToggle {
+    constructor(el) {
+        this.el = el
+
+        var modal = document.querySelectorAll('.popup')[0];
+        console.log(modal);
+        
+
+        this.el.addEventListener('click', function(event){
+            modal.classList.toggle("fadeInDown");
+        })
+    }
+}
