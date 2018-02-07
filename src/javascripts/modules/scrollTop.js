5ec3d6ec8f17467cc43103771728fee90d8ad0f8
joshuarte <joshuarte@users.noreply.github.com>
2018-02-07 23:10:47 +0100

    bugfix minori

--- src/javascripts/modules/scrollTop.js
+++ src/javascripts/modules/scrollTop.js
@@ -11,7 +11,6 @@ export default class scrollTop {
         }
 
         this.el = el
-        /* console.log('scrollTop module loaded'); */
 
         this.el.addEventListener('click', function (event) {
             jump('#page-top', {
